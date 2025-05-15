bb437ed4612593bb9581fd412db821ccad37e317
AndresZ25 <andres.zun25@gmail.com>
2025-05-15 11:11:55 -0400

    change 2 .env

--- config/connection.js
+++ config/connection.js
@@ -4,33 +4,16 @@ require('dotenv').config();
 let sequelize;
 
 if (process.env.DB_URL) {
-  sequelize = new Sequelize(process.env.DB_URL, {
-    dialect: 'postgres',
-    protocol: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, 
-      },
-    },
-  });
+  sequelize = new Sequelize(process.env.DB_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: process.env.DB_HOST,
+      host: 'localhost',
       dialect: 'postgres',
-      port: 5432,
-      dialectOptions: {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false,
-        },
-      },
-    }
+    },
   );
 }
-
 module.exports = sequelize;
